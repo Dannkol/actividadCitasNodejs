27ad9fe1f71e645e1ac4338ed571308b3b05a8fe
Daniel Manosalva <Manosalvasierra@gmail.com>
2023-07-12 08:49:59 -0500

    feat: :construction: Tercer endpoint listo, con todo lo necesario para funcionar
    3. Obtener todos los médicos de una especialidad específica (por ejemplo, **'Cardiología'**):

--- src/routes.js
+++ src/routes.js
@@ -57,7 +57,28 @@ router.get("/citas", async (req, res) => {
 });
 
 
+router.get("/medicos/:esp", async (req, res) => {
+  const connection = await getConnection();
+  try {
+    const query_pacientes_all_desc = `SELECT t1.med_nombreCompleto , t2.esp_nombre FROM medico AS t1 INNER JOIN especialidad AS t2 ON t1.med_especialidad = t2.esp_id WHERE t2.esp_nombre = "${req.params.esp}"; `;
 
+    const [pacientes_all_desc] = await connection.execute(
+      query_pacientes_all_desc
+    );
+
+    let obj = {
+      mensaje: "citas ordered by cit_fecha DESC",
+      inventario: pacientes_all_desc,
+    };
+
+    return res.status(200).json(obj);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: `Error del servidor ${error.errno}` });
+  } finally {
+    connection.end();
+  }
+});
 
 
 export default router;
