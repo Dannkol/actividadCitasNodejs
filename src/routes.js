088a3e7054f6b0354a2d9fc88b9adc7f2095010c
Daniel Manosalva <manosalvasierra@gmail.com>
2023-07-12 17:09:28 -0500

    feat: :construction: decimo endpoint terminado
    10. Obtener los consultorio donde se aplicó las citas de un paciente

--- src/routes.js
+++ src/routes.js
@@ -243,6 +243,35 @@ WHERE t1.cit_fecha = '${req.params.date}' AND t3.med_nroMatriculaProsional = ${r
 });
 
 
+router.get("/usuarios/finalizado/consultorio/:id", Validar_id ,async (req, res) => {
+  const connection = await getConnection();
+  try {
+    const query_pacientes_all_desc = `
+    SELECT t1.cit_fecha , t4.cons_nombre FROM cita AS t1 
+INNER JOIN usuarios AS t2 ON t1.cit_datosUsuario = t2.usu_id 
+INNER JOIN medico AS t3 ON t1.cit_medico = t3.med_nroMatriculaProsional
+INNER JOIN consultorio AS t4 ON t3.med_consultorio = t4.cons_codigo
+WHERE t2.usu_id = 1 AND t1.cit_estado = 1;
+    `;
+
+    const [pacientes_all_desc] = await connection.execute(
+      query_pacientes_all_desc
+    );
+
+    let obj = {
+      mensaje: "cantidad de Citas",
+      data: pacientes_all_desc,
+    };
+
+    return res.status(200).json(obj);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: `Error del servidor ${error.errno}` });
+  } finally {
+    connection.end();
+  }
+});
+
 
 
 export default router;
